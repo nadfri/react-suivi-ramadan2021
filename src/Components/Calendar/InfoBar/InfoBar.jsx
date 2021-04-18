aa7e0772e73ea09cca26407b557d8bcb390f7bac
Nader Frigui <nadfri@gmail.com>
2021-04-18 17:27:54 +0200

    ajout du toggle Calendrier

--- src/Components/Calendar/InfoBar/InfoBar.jsx
+++ src/Components/Calendar/InfoBar/InfoBar.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import "./InfoBar.scss"
+import { useState } from 'react';
+import './InfoBar.scss';
 
 function InfoBar() {
+	const options = { day: 'numeric', month: 'long', weekday: 'long', year: 'numeric' };
+	const islamic = new Intl.DateTimeFormat('fr-FR-u-ca-islamic', options).format(Date.now());
+	const french  = new Intl.DateTimeFormat('fr-FR', options).format(Date.now());
 
-    
+	//State
+	const [toggle, setToggle] = useState(true);
 
-    return (
-        <div className="InfoBar">
-            <span className="date">
-                {new Intl.DateTimeFormat('fr-FR-u-ca-islamic', {day: 'numeric', month: 'long',weekday: 'long',year : 'numeric'}).format(Date.now())}
-                </span>
-        </div>
-    );
+	return (
+		<div className='InfoBar' onClick={() => setToggle(!toggle)}>
+			<span className='date'>
+				{toggle? islamic: french}
+			</span>
+		</div>
+	);
 }
 
 export default InfoBar;
