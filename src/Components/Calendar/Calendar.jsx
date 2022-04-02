a5215388b2bc1a7ccd086c9d1d901f0460556740
Nader Frigui <nadfri@gmail.com>
2022-04-02 16:58:24 +0200

    Ajout Modal Error connexion

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,174 +1,179 @@
 //Librairies
-import { db } from '../../firebase';
-import React, { useState, useEffect } from 'react';
+import { db } from "../../firebase";
+import React, { useState, useEffect } from "react";
 //CSS
-import './Calendar.scss';
-import settingIco from './Settings/settings.png';
+import "./Calendar.scss";
+import settingIco from "./Settings/settings.png";
 //Components
-import Day from './Day/Day';
-import InfoBar from './InfoBar/InfoBar';
-import Loader from '../Loader/Loader';
-import Settings from './Settings/Settings';
-import Total from './Total/Total';
+import Day from "./Day/Day";
+import InfoBar from "./InfoBar/InfoBar";
+import Loader from "../Loader/Loader";
+import Settings from "./Settings/Settings";
+import Total from "./Total/Total";
+import ErrorModal from "../ErrorModal/ErrorModal";
 
+function Calendar({ user, setWallpaper, wallpaper }) {
+  /*State*/
+  const [displaySettings, setDisplaySettings] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [state, setState] = useState(null);
+  const [jeuner, setJeuner] = useState(0);
+  const [manquer, setManquer] = useState(0);
+  const [pertePoids, setPertePoids] = useState(0);
+  const [error, setError] = useState(false);
 
-function Calendar({user,setWallpaper,wallpaper}) {
+  /*Gestion du Settings*/
+  const changeFirstPoids = (value) =>
+    setState((prev) => ({ ...prev, firstPoids: value }));
+  const changeFirstDay = (value) => setState((prev) => ({ ...prev, firstDay: value }));
+  const changeFirstConnect = (value) =>
+    setState((prev) => ({ ...prev, firstConnect: value }));
+  const changeDisplaySettings = (value) => setDisplaySettings(value);
 
+  /*Chargement des données*/
+  useEffect(() => {
+    setLoader(true);
+    db.collection("users")
+      .doc(user.uid)
+      .get()
+      .then((doc) => {
+        setState(doc.data());
+        setLoader(false);
+        if (doc.data().firstConnect) setDisplaySettings(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoader(false);
+        setError(true);
+      });
 
-	/*State*/
-	const [displaySettings, setDisplaySettings] = useState(false);
-	const [loader, setLoader] 		            = useState(false);
-	const [state, setState] 		            = useState(null);
-	const [jeuner, setJeuner]                   = useState(0);
-	const [manquer, setManquer]                 = useState(0);
-	const [pertePoids, setPertePoids]           = useState(0);
+    //WillUnmount
+    return () => console.log("CleanUp");
+  }, [user]);
 
-	/*Gestion du Settings*/
-	const changeFirstPoids      = (value) => setState((prev) => ({ ...prev, firstPoids: value }));
-	const changeFirstDay        = (value) => setState((prev) => ({ ...prev, firstDay: value }));
-	const changeFirstConnect    = (value) => setState((prev) => ({ ...prev, firstConnect: value }));
-	const changeDisplaySettings = (value) => setDisplaySettings(value);
+  /*Mise à jour des Totaux*/
+  useEffect(() => {
+    if (state) {
+      let countValid = 0;
+      let countNotValid = 0;
+      state.jours.forEach((jour) => {
+        if (jour.valid) countValid++;
+        else if (!jour.Valid && jour.checked) countNotValid++;
+      });
 
-	/*Chargement des données*/
-	useEffect(() => {
-		setLoader(true);
-		db.collection('users')
-			.doc(user.uid)
-			.get()
-			.then((doc) => {
-				setState(doc.data());
-				setLoader(false);
-				if (doc.data().firstConnect) setDisplaySettings(true);
-			})
-			.catch((err) => console.log(err));
+      setJeuner(countValid);
+      setManquer(countNotValid);
 
-		//WillUnmount	
-		return () => console.log('CleanUp');
-	}, [user]);
+      /*Mise à jour du Poids*/
+      if (state.firstPoids) {
+        const tabKgs = state.jours.filter((jour) => jour.poids);
+        if (tabKgs.length < 1) setPertePoids(0);
+        else {
+          let total = (-(state.firstPoids - tabKgs[tabKgs.length - 1].poids)).toFixed(1);
+          total = total > 0 ? `+${total}` : total;
+          setPertePoids(total);
+        }
+      }
+    }
+  }, [state]);
 
-	/*Mise à jour des Totaux*/
-	useEffect(() => {
-		if (state) {
-			let countValid = 0;
-			let countNotValid = 0;
-			state.jours.forEach((jour) => {
-				if (jour.valid) countValid++;
-				else if (!jour.Valid && jour.checked) countNotValid++;
-			});
+  /*Mise en place du calendrier*/
+  function calendrier(state) {
+    return state.jours.map((day, index) => {
+      let firstDay = new Date(state.firstDay);
+      let date = new Date();
+      date.setTime(firstDay.getTime() + index * 1000 * 3600 * 24); //better than setDate
+      date = new Intl.DateTimeFormat("fr-FR", {
+        year: "2-digit",
+        month: "2-digit",
+        day: "2-digit",
+      }).format(date);
 
-			setJeuner(countValid);
-			setManquer(countNotValid);
+      return (
+        <Day
+          key={index}
+          date={date}
+          state={day}
+          firstPoids={state.firstPoids}
+          index={index}
+          changeDayPoids={changeDayPoids}
+          checkValidDay={checkValidDay}
+        />
+      );
+    });
+  }
 
-	/*Mise à jour du Poids*/
-			if (state.firstPoids) 
-			{
-				const tabKgs = state.jours.filter((jour) => jour.poids);
-				if (tabKgs.length < 1) setPertePoids(0);
-				else 
-				{
-					let total = (-(state.firstPoids - tabKgs[tabKgs.length - 1].poids)).toFixed(1);
-					total = total > 0 ? `+${total}` : total;
-					setPertePoids(total);
-				}
-			}
-		}
-	}, [state]);
+  /*Fonction Mise à jour via Day/Modale */
+  const changeDayPoids = (index, value) => {
+    const copyState = { ...state };
+    copyState.jours[index].poids = value;
+    setState(copyState);
+    db.collection("users").doc(user.uid).update(copyState);
+  };
 
-	/*Mise en place du calendrier*/
-	function calendrier(state) {
-		return state.jours.map((day, index) => {
-			let firstDay = new Date(state.firstDay);
-			let date = new Date();
-			date.setTime(firstDay.getTime() + index*1000*3600*24); //better than setDate
-			date = new Intl.DateTimeFormat('fr-FR',{
-			year: "2-digit",
-			month: "2-digit",
-			day: "2-digit"}).format(date);
+  const checkValidDay = (index, valid, check) => {
+    const copyState = { ...state };
+    copyState.jours[index].valid = valid;
+    copyState.jours[index].checked = check;
+    setState(copyState);
+    db.collection("users").doc(user.uid).update(copyState);
+  };
 
-			return (
-				<Day
-					key={index}
-					date={date}
-					state={day}
-					firstPoids={state.firstPoids}
-					index={index}
-					changeDayPoids={changeDayPoids}
-					checkValidDay={checkValidDay}
-				/>
-			);
-		});
-	}
+  /*Suppression De la Base de Données*/
+  const suppressionDB = () => {
+    const copyState = { ...state };
+    copyState.firstConnect = true;
+    copyState.firstPoids = null;
 
-	/*Fonction Mise à jour via Day/Modale */
-	const changeDayPoids = (index, value) => {
-		const copyState = { ...state };
-		copyState.jours[index].poids = value;
-		setState(copyState);
-		db.collection('users').doc(user.uid).update(copyState);
-	};
+    for (let jour of copyState.jours) {
+      jour.poids = null;
+      jour.valid = false;
+      jour.checked = false;
+    }
 
-	const checkValidDay = (index, valid, check) => {
-		const copyState = { ...state };
-		copyState.jours[index].valid = valid;
-		copyState.jours[index].checked = check;
-		setState(copyState);
-		db.collection('users').doc(user.uid).update(copyState);
-	};
+    db.collection("users").doc(user.uid).set(copyState);
+    setState(copyState);
+    setDisplaySettings(true);
+  };
 
+  /********************Rendu JSX********************/
+  return (
 
-	/*Suppression De la Base de Données*/
-	const suppressionDB = () => {
-		const copyState = {...state};
-		copyState.firstConnect = true;
-		copyState.firstPoids = null;
+      <div className="Calendar">
+        {loader && <Loader />}
 
-		for (let jour of copyState.jours)
-		{ 
-			jour.poids   = null;
-			jour.valid   = false;
-			jour.checked = false;
-		}
+				{error && <ErrorModal />}
+        <img
+          src={settingIco}
+          className="settingIco"
+          alt="parameters"
+          onClick={() => changeDisplaySettings(true)}
+        />
 
-		db.collection('users').doc(user.uid).set(copyState);
-		setState(copyState);
-		setDisplaySettings(true)
-		
-	};
+        {displaySettings ? (
+          <Settings
+            firstDay={state.firstDay}
+            firstPoids={state.firstPoids}
+            changeFirstDay={changeFirstDay}
+            changeFirstPoids={changeFirstPoids}
+            changeDisplaySettings={changeDisplaySettings}
+            changeFirstConnect={changeFirstConnect}
+            suppressionDB={suppressionDB}
+            user={user}
+            setWallpaper={setWallpaper}
+            wallpaper={wallpaper}
+          />
+        ) : null}
+        <InfoBar />
+        {state && (
+          <div className="grid">
+            {calendrier(state)}{" "}
+            <Total jeuner={jeuner} manquer={manquer} pertePoids={pertePoids} />
+          </div>
+        )}
+      </div>
 
-	/********************Rendu JSX********************/
-	return (
-		<div className='Calendar'>
-			{loader && <Loader />}
-			<img
-				src={settingIco}
-				className='settingIco'
-				alt='parameters'
-				onClick={() => changeDisplaySettings(true)}
-			/>
-
-			{displaySettings ? (
-				<Settings
-					firstDay={state.firstDay}
-					firstPoids={state.firstPoids}
-					changeFirstDay={changeFirstDay}
-					changeFirstPoids={changeFirstPoids}
-					changeDisplaySettings={changeDisplaySettings}
-					changeFirstConnect={changeFirstConnect}
-					suppressionDB={suppressionDB}
-					user={user}
-					setWallpaper={setWallpaper}
-					wallpaper= {wallpaper}
-				/>
-			) : null}
-			<InfoBar />
-			{state && (
-				<div className='grid'>
-					{calendrier(state)}{' '}
-					<Total jeuner={jeuner} manquer={manquer} pertePoids={pertePoids} />
-				</div>
-			)}
-		</div>
-	);
+  );
 }
 
 export default Calendar;
---
+++ src/Components/ErrorModal/ErrorModal.jsx
@@ -0,0 +1,11 @@
+import './ErrorModal.scss';
+
+export default function ErrorModal() {
+  return (
+    <div className="ErrorModal">
+      <div className="container">
+        Probleme de Connexion, Veuillez réessayer
+      </div>
+    </div>
+  )
+}
