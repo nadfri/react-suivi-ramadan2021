3e55781a61247e31e9ea1b396984f9283631d223
Nader Frigui <nadfri@gmail.com>
2026-02-17 19:00:35 +0100

    Set Ramadan start 2026-02-18

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
 export const USERS = process.env.NODE_ENV === 'development' ? 'users-test' : 'users';
-export const FIRST_DAY = '2025-03-01'; /* YYYY-MM-DD */
+export const FIRST_DAY = '2026-02-18'; /* YYYY-MM-DD */
